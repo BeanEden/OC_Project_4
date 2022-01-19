2f54473663df3114e2c012daad9082a548489c99
JC <bean3den@gmail.com>
2022-01-19 08:55:09 +0100

    simplification modèle player

--- Models/players.py
+++ Models/players.py
@@ -18,35 +18,36 @@ class Player :
         "rank" : self.rank
     })
 
+    def serialization_player(self,family_name, first_name, birth_date, gender, rank, score=0):
+
+        serialized_player = {
+            "family_name": Player.family_name,
+            "first_name": Player.first_name,
+            "birth_date": Player.birth_date,
+            "gender": Player.gender,
+            "rank": Player.rank,
+            "score": Player.score
+        }
+        return serialized_player
+
     def add_a_player(self):
         """Prompt for adding a player."""
-        print("Création d'un nouveau joueur:")
+        print("Création d'un nouveau joueur...")
         self.family_name = input("Nom du joueur : ")
         self.first_name = input("Prénom du joueur: ")
         self.birth_date = input("Date de naissance du joueur (DD/MM/YYYY): ")
         self.gender = input("Genre (F/H): ")
         self.rank = input("Classement (chiffre positif) : ")
-        return print("Player ",self.first_name, " ", self.family_name, " a bien été enregistré(e)")
-
-    def serialization_player(self,family_name, first_name, birth_date, gender, rank, score=0):
-
-        serialized_player = {
-            "family_name" : Player.family_name,
-            "first_name" : Player.first_name,
-            "birth_date" : Player.birth_date,
-            "gender" : Player.gender,
-            "rank" : Player.rank,
-            "score" : Player.score
-        }
-        return serialized_player
-
-# from tinydb import TinyDB
-#
-# db = TinyDB(‘db.json’)
-# players_table = db.table("players")
-# players_table.truncate()	# clear the table first
-# players_table.insert_multiple(serialized_players)
-#
-# serialized_players = players_table.all()
+        print("Player ",self.first_name, " ", self.family_name, " a bien été enregistré(e)")
+        player_dictionary = Player.serialization_player(
+            self,
+            self.family_name,
+            self.first_name,
+            self.birth_date,
+            self.gender,
+            self.rank,)
+        return player_dictionary
 
 
+joueur_1 =
+print(Player.add_a_player("Joueur1"))
